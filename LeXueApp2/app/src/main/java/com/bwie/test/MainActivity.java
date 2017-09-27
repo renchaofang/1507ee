f07546a6bbe4787996745b886d61c0af4106328e
renchaofang <3216829641@qq.com>
2017-09-27 16:34:54 +0800

    提交0927

--- LeXueApp2/app/src/main/java/com/bwie/test/MainActivity.java
+++ LeXueApp2/app/src/main/java/com/bwie/test/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //这是我们的第一次相爱
     }
 }
